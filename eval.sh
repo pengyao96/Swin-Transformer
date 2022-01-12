08f66aed518db7df4d3ea13dffc3814b2c0a4767
yaopeng <yaopeng@kuaishou.com>
2022-01-12 16:05:04 +0800

    add eval.sh

--- eval.sh
+++ eval.sh
@@ -5,5 +5,5 @@ config_name="swin_small_patch4_window7_224.yaml"
 data_path="/imagenet/imagenet/ILSVRC/Data/CLS-LOC"
 
 
-python -m torch.distributed.launch --nproc_per_node 8 --master_port 12345 main.py --eval \
+python -m torch.distributed.launch --nproc_per_node 4 --master_port 12345 main.py --eval \
 --cfg configs/${config_name} --resume swin_base_patch4_window7_224.pth --data-path ${data_path}
