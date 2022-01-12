a160dc9dbcc0091277e2f59dbfabc3974fa8df64
yaopeng <yaopeng@kuaishou.com>
2022-01-12 18:14:59 +0800

    merge new official code

--- finetune.sh
+++ finetune.sh
@@ -4,5 +4,5 @@ python -m torch.distributed.launch --nproc_per_node 8 --master_port 12345  main.
 
 
 python -m torch.distributed.launch --nproc_per_node 8 --master_port 12345  main.py \
---cfg configs/swin_tiny_patch4_window7_224.yaml --pretrained swin_tiny_patch4_window7_224.pth \
+--cfg configs/swin_tiny_patch4_window7_224.yaml --pretrained /home/yaopeng/Code/model_checkpoint/swin2d/swin_tiny_patch4_window7_224.pth \
 --data-path /imagenet/imagenet/ILSVRC/Data/CLS-LOC --batch-size 128 --tag hammer_tune
