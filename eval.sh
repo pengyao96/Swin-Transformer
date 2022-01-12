e8a3847d0fedcb4085284140c8bb785b6a765482
yaopeng <yaopeng@kuaishou.com>
2022-01-12 16:23:09 +0800

    add eval.sh

--- eval.sh
+++ eval.sh
@@ -1,10 +1,10 @@
 config_name="swin_tiny_patch4_window7_224.yaml"
+resume="/home/yaopeng/Code/model_checkpoint/swin2d/swin_tiny_patch4_window7_224.pth"
+
 #config_name="swin_small_patch4_window7_224.yaml"
 #config_name="swin_base_patch4_window7_224.yaml"
 
 data_path="/imagenet/imagenet/ILSVRC/Data/CLS-LOC"
-
-resume="swin_base_patch4_window7_224.pth"
 
 
 python -m torch.distributed.launch --nproc_per_node 8 --master_port 12345 main.py --eval \
