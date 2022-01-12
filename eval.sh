bfb614783fa68c63ba12bfa6db08428b55476017
yaopeng <yaopeng@kuaishou.com>
2022-01-12 16:28:10 +0800

    add eval.sh

--- eval.sh
+++ eval.sh
@@ -1,9 +1,12 @@
 #config_name="swin_tiny_patch4_window7_224.yaml"
 #resume="/home/yaopeng/Code/model_checkpoint/swin2d/swin_tiny_patch4_window7_224.pth"
 
-config_name="swin_small_patch4_window7_224.yaml"
-resume="/home/yaopeng/Code/model_checkpoint/swin2d/swin_small_patch4_window7_224.pth"
-#config_name="swin_base_patch4_window7_224.yaml"
+#config_name="swin_small_patch4_window7_224.yaml"
+#resume="/home/yaopeng/Code/model_checkpoint/swin2d/swin_small_patch4_window7_224.pth"
+
+config_name="swin_base_patch4_window7_224.yaml"
+resume="/home/yaopeng/Code/model_checkpoint/swin2d/swin_base_patch4_window7_224.pth"
+
 
 data_path="/imagenet/imagenet/ILSVRC/Data/CLS-LOC"
 
